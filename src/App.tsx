594b9f4198dc83b34216b2cfb1620e266cc822be
larissaolimpio <lrsolimpio@gmail.com>
2026-02-28 17:51:49 -0300

    Create: sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import { Sidebar } from "./assets/components/sidebar/Sidebar";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello word</h1>
+    <div className="flex">
+      <Sidebar></Sidebar>
+      <div className="flex-1 min-h-screen bg-blue-200 flex items-center justify-center">
+        <h1>Smart Finance</h1>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/assets/components/sidebar/Sidebar.tsx
@@ -0,0 +1,56 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { GrMoney } from "react-icons/gr";
+import { LuLogOut, LuTrendingDown } from "react-icons/lu";
+import { RxDashboard } from "react-icons/rx";
+import { Tooltip } from "react-tooltip";
+
+interface MenuItem {
+  name: string;
+  icon: React.ReactNode;
+}
+export function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuItems: MenuItem[] = [
+    { name: "Dashboard", icon: <RxDashboard /> },
+    { name: "Income", icon: <GrMoney /> },
+    { name: "Expenses", icon: <LuTrendingDown /> },
+    { name: "Logout", icon: <LuLogOut /> },
+  ];
+  const mapMenuItems = menuItems.map((item) => (
+    <div
+      key={item.name}
+      className="flex items-center gap-5 cursor-pointer hover:bg-gray-700 rounded px-5 py-2"
+    >
+      <span
+        onClick={() => setIsOpen((prev) => !prev)}
+        data-tooltip-id={!isOpen ? "sidebar-tooltip" : undefined}
+        data-tooltip-content={!isOpen ? item.name : undefined}
+      >
+        {item.icon}
+      </span>
+      {isOpen && <span>{item.name}</span>}
+    </div>
+  ));
+  return (
+    <div>
+      <motion.div
+        initial={{ width: 60 }}
+        animate={{ width: isOpen ? 240 : 60 }}
+        transition={{ duration: 0.4 }}
+        className="w-64 h-screen bg-gray-800 text-white "
+      >
+        {" "}
+        <button
+          className="mb-10 p-5"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <FaBars />
+        </button>
+        <nav className=" flex flex-col gap-11">{mapMenuItems}</nav>
+      </motion.div>
+      {!isOpen && <Tooltip id="sidebar-tooltip" offset={30} />}
+    </div>
+  );
+}
